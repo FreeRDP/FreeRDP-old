26098d04151bc46f6caf906c9e545ec3a736de1b
Jay Sorg <jay.sorg@gmail.com>
2009-10-21 23:16:36 +0000

    fill out hexdump for debugging
    git-svn-id: https://freerdp.svn.sourceforge.net/svnroot/freerdp/trunk@163 7ffbcff5-ea86-4e99-a76d-b9f7f6476058

--- freerdp.c
+++ freerdp.c
@@ -81,6 +81,29 @@ ui_unimpl(void * inst, char * format, ...)
 void
 hexdump(unsigned char * p, unsigned int len)
 {
+	unsigned char *line = p;
+	int i, thisline, offset = 0;
+
+	while (offset < len)
+	{
+		printf("%04x ", offset);
+		thisline = len - offset;
+		if (thisline > 16)
+			thisline = 16;
+
+		for (i = 0; i < thisline; i++)
+			printf("%02x ", line[i]);
+
+		for (; i < 16; i++)
+			printf("   ");
+
+		for (i = 0; i < thisline; i++)
+			printf("%c", (line[i] >= 0x20 && line[i] < 0x7f) ? line[i] : '.');
+
+		printf("\n");
+		offset += thisline;
+		line += thisline;
+	}
 }
 
 int
