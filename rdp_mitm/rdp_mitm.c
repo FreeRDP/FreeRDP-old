c388616fd044c5e14761b383970f801d0910bbae
Marc-Andre Moreau <marcandre.moreau@gmail.com>
2009-11-15 21:56:36 +0000

    Deeper processing of the NTLMSSP authentication messages
    git-svn-id: https://freerdp.svn.sourceforge.net/svnroot/freerdp/trunk@189 7ffbcff5-ea86-4e99-a76d-b9f7f6476058

--- rdp_mitm/rdp_mitm.c
+++ rdp_mitm/rdp_mitm.c
@@ -30,6 +30,9 @@
 	real server after. This may require the proxy to know in advance the real password.
 */
 
+#include "rdesktop.h"
+#include "mem.h"
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -87,7 +90,416 @@ unsigned char clientInitial[] =
 unsigned char serverInitial[] =
 	"\x03\x00\x00\x13\x0e\xd0\x00\x00\x12\x34\x00\x02\x00\x08\x00\x02\x00\x00\x00";
 
-void hexdump(char* bytes, int length);
+typedef struct _NEGOTIATE
+{
+	uint32 flags;
+	uint8 NTLMSSP_NEGOTIATE_56;
+	uint8 NTLMSSP_NEGOTIATE_KEY_EXCH;
+	uint8 NTLMSSP_NEGOTIATE_128;
+	uint8 NTLMSSP_NEGOTIATE_VERSION;
+	uint8 NTLMSSP_NEGOTIATE_TARGET_INFO;
+	uint8 NTLMSSP_REQUEST_NON_NT_SESSION_KEY;
+	uint8 NTLMSSP_NEGOTIATE_IDENTIFY;
+	uint8 NTLMSSP_NEGOTIATE_EXTENDED_SESSIONSECURITY;
+	uint8 NTLMSSP_TARGET_TYPE_SHARE;
+	uint8 NTLMSSP_TARGET_TYPE_SERVER;
+	uint8 NTLMSSP_TARGET_TYPE_DOMAIN;
+	uint8 NTLMSSP_NEGOTIATE_ALWAYS_SIGN;
+	uint8 NTLMSSP_NEGOTIATE_OEM_WORKSTATION_SUPPLIED;
+	uint8 NTLMSSP_NEGOTIATE_OEM_DOMAIN_SUPPLIED;
+	uint8 NTLMSSP_NEGOTIATE_NT_ONLY;
+	uint8 NTLMSSP_NEGOTIATE_NTLM;
+	uint8 NTLMSSP_NEGOTIATE_LM_KEY;
+	uint8 NTLMSSP_NEGOTIATE_DATAGRAM;
+	uint8 NTLMSSP_NEGOTIATE_SEAL;
+	uint8 NTLMSSP_NEGOTIATE_SIGN;
+	uint8 NTLMSSP_REQUEST_TARGET;
+	uint8 NTLMSSP_NEGOTIATE_OEM;
+	uint8 NTLMSSP_NEGOTIATE_UNICODE;
+}
+NEGOTIATE;
+
+void print_wstr(char* bytes, int length);
+void print_hexdump(char* bytes, int length);
+void ntlm_recv_negotiate_message(STREAM s);
+void ntlm_recv_challenge_message(STREAM s);
+void ntlm_recv_authenticate_message(STREAM s);
+void ntlm_recv_negotiate_flags(STREAM s, NEGOTIATE* negotiate);
+
+void ntlm_recv_negotiate_flags(STREAM s, NEGOTIATE* negotiate)
+{
+	in_uint32_be(s, negotiate->flags);
+
+	negotiate->NTLMSSP_NEGOTIATE_56 = negotiate->flags >> 0 & 1;
+	negotiate->NTLMSSP_NEGOTIATE_KEY_EXCH = negotiate->flags >> 1 & 1;
+	negotiate->NTLMSSP_NEGOTIATE_128 = negotiate->flags >> 2 & 1;
+	negotiate->NTLMSSP_NEGOTIATE_VERSION = negotiate->flags >> 6 & 1;
+	negotiate->NTLMSSP_NEGOTIATE_TARGET_INFO = negotiate->flags >> 8 & 1;
+	negotiate->NTLMSSP_REQUEST_NON_NT_SESSION_KEY = negotiate->flags >> 9 & 1;
+	negotiate->NTLMSSP_NEGOTIATE_IDENTIFY = negotiate->flags >> 11 & 1;
+	negotiate->NTLMSSP_NEGOTIATE_EXTENDED_SESSIONSECURITY = negotiate->flags >> 12 & 1;
+	negotiate->NTLMSSP_TARGET_TYPE_SHARE = negotiate->flags >> 13 & 1;
+	negotiate->NTLMSSP_TARGET_TYPE_SERVER = negotiate->flags >> 14 & 1;
+	negotiate->NTLMSSP_TARGET_TYPE_DOMAIN = negotiate->flags >> 15 & 1;
+	negotiate->NTLMSSP_NEGOTIATE_ALWAYS_SIGN = negotiate->flags >> 16 & 1;
+	negotiate->NTLMSSP_NEGOTIATE_OEM_WORKSTATION_SUPPLIED = negotiate->flags >> 18 & 1;
+	negotiate->NTLMSSP_NEGOTIATE_OEM_DOMAIN_SUPPLIED = negotiate->flags >> 19 & 1;
+	negotiate->NTLMSSP_NEGOTIATE_NT_ONLY = negotiate->flags >> 21 & 1;
+	negotiate->NTLMSSP_NEGOTIATE_NTLM = negotiate->flags >> 22 & 1;
+	negotiate->NTLMSSP_NEGOTIATE_LM_KEY = negotiate->flags >> 24 & 1;
+	negotiate->NTLMSSP_NEGOTIATE_DATAGRAM = negotiate->flags >> 25 & 1;
+	negotiate->NTLMSSP_NEGOTIATE_SEAL = negotiate->flags >> 26 & 1;
+	negotiate->NTLMSSP_NEGOTIATE_SIGN = negotiate->flags >> 27 & 1;
+	negotiate->NTLMSSP_REQUEST_TARGET = negotiate->flags >> 29 & 1;
+	negotiate->NTLMSSP_NEGOTIATE_OEM = negotiate->flags >> 30 & 1;
+	negotiate->NTLMSSP_NEGOTIATE_UNICODE = negotiate->flags >> 31 & 1;
+
+	if(negotiate->NTLMSSP_NEGOTIATE_56)
+		printf("NTLMSSP_NEGOTIATE_56\n");
+	if(negotiate->NTLMSSP_NEGOTIATE_KEY_EXCH)
+		printf("NTLMSSP_NEGOTIATE_KEY_EXCH\n");
+	if(negotiate->NTLMSSP_NEGOTIATE_128)
+		printf("NTLMSSP_NEGOTIATE_128\n");
+	if(negotiate->NTLMSSP_NEGOTIATE_VERSION)
+		printf("NTLMSSP_NEGOTIATE_VERSION\n");
+	if(negotiate->NTLMSSP_NEGOTIATE_TARGET_INFO)
+		printf("NTLMSSP_NEGOTIATE_TARGET_INFO\n");
+	if(negotiate->NTLMSSP_REQUEST_NON_NT_SESSION_KEY)
+		printf("NTLMSSP_REQUEST_NON_NT_SESSION_KEY\n");
+	if(negotiate->NTLMSSP_NEGOTIATE_IDENTIFY)
+		printf("NTLMSSP_NEGOTIATE_IDENTIFY\n");
+	if(negotiate->NTLMSSP_NEGOTIATE_EXTENDED_SESSIONSECURITY)
+		printf("NTLMSSP_NEGOTIATE_EXTENDED_SESSIONSECURITY\n");
+	if(negotiate->NTLMSSP_TARGET_TYPE_SHARE)
+		printf("NTLMSSP_TARGET_TYPE_SHARE\n");
+	if(negotiate->NTLMSSP_TARGET_TYPE_SERVER)
+		printf("NTLMSSP_TARGET_TYPE_SERVER\n");
+	if(negotiate->NTLMSSP_TARGET_TYPE_DOMAIN)
+		printf("NTLMSSP_TARGET_TYPE_DOMAIN\n");
+	if(negotiate->NTLMSSP_NEGOTIATE_ALWAYS_SIGN)
+		printf("NTLMSSP_NEGOTIATE_ALWAYS_SIGN\n");
+	if(negotiate->NTLMSSP_NEGOTIATE_OEM_WORKSTATION_SUPPLIED)
+		printf("NTLMSSP_NEGOTIATE_OEM_WORKSTATION_SUPPLIED\n");
+	if(negotiate->NTLMSSP_NEGOTIATE_OEM_DOMAIN_SUPPLIED)
+		printf("NTLMSSP_NEGOTIATE_OEM_DOMAIN_SUPPLIED\n");
+	if(negotiate->NTLMSSP_NEGOTIATE_NT_ONLY)
+		printf("NTLMSSP_NEGOTIATE_NT_ONLY\n");
+	if(negotiate->NTLMSSP_NEGOTIATE_NTLM)
+		printf("NTLMSSP_NEGOTIATE_NTLM\n");
+	if(negotiate->NTLMSSP_NEGOTIATE_LM_KEY)
+		printf("NTLMSSP_NEGOTIATE_LM_KEY\n");
+	if(negotiate->NTLMSSP_NEGOTIATE_DATAGRAM)
+		printf("NTLMSSP_NEGOTIATE_DATAGRAM\n");
+	if(negotiate->NTLMSSP_NEGOTIATE_SEAL)
+		printf("NTLMSSP_NEGOTIATE_SEAL\n");
+	if(negotiate->NTLMSSP_NEGOTIATE_SIGN)
+		printf("NTLMSSP_NEGOTIATE_SIGN\n");
+	if(negotiate->NTLMSSP_REQUEST_TARGET)
+		printf("NTLMSSP_REQUEST_TARGET\n");
+	if(negotiate->NTLMSSP_NEGOTIATE_OEM)
+		printf("NTLMSSP_NEGOTIATE_OEM\n");
+	if(negotiate->NTLMSSP_NEGOTIATE_UNICODE)
+		printf("NTLMSSP_NEGOTIATE_UNICODE\n");
+}
+
+void ntlmssp_recv(STREAM s)
+{
+	char signature[8]; /* Signature, "NTLMSSP" */
+	uint32 messageType; /* MessageType */
+
+	in_uint8a(s, signature, 8);
+	printf("Signature: %s\n", signature);
+	in_uint32_le(s, messageType);
+
+	printf("Message Type: %X\n", messageType);
+	switch (messageType)
+	{
+		/* NEGOTIATE_MESSAGE */
+		case 0x00000001:
+			printf("NEGOTIATE_MESSAGE\n");
+			ntlm_recv_negotiate_message(s);
+			break;
+
+		/* CHALLENGE_MESSAGE */
+		case 0x00000002:
+			printf("CHALLENGE_MESSAGE\n");
+			ntlm_recv_challenge_message(s);
+			break;
+
+		/* AUTHENTICATE_MESSAGE */
+		case 0x00000003:
+			printf("AUTHENTICATE_MESSAGE\n");
+			ntlm_recv_authenticate_message(s);
+			break;
+	}
+}
+
+void ntlm_recv_negotiate_message(STREAM s)
+{
+	/* A NEGOTIATE data structure */
+	NEGOTIATE negotiateFlags;
+
+	/* DomainNameFields (8 bytes) */
+	uint16 domainNameLen;
+	uint16 domainNameMaxLen;
+	uint32 domainNameBufferOffset;
+
+	/* WorkstationFields (8 bytes) */
+	uint16 workstationLen;
+	uint16 workstationMaxLen;
+	uint32 workstationBufferOffset;
+
+	/* Payload */
+	char* domainName = NULL;
+	char* workstationName = NULL;
+
+	ntlm_recv_negotiate_flags(s, &negotiateFlags);
+
+	in_uint16_le(s, domainNameLen);
+	in_uint16_le(s, domainNameMaxLen);
+	in_uint32_le(s, domainNameBufferOffset);
+
+	in_uint16_le(s, workstationLen);
+	in_uint16_le(s, workstationMaxLen);
+	in_uint16_le(s, workstationBufferOffset);
+
+	/* Version, VERSION data structure (8 bytes) */
+	/* [MS-NLMP] says it should be ignored */
+	in_uint8s(s, 8); /* Skip */
+
+	if(negotiateFlags.NTLMSSP_NEGOTIATE_OEM_DOMAIN_SUPPLIED)
+	{
+		domainName = xmalloc(domainNameLen);
+		s->p = s->data + domainNameBufferOffset;
+		in_uint8a(s, domainName, domainNameLen);
+		printf("Domain name: ");
+		print_wstr(domainName, domainNameLen);
+		printf("\n");
+	}
+
+	if(negotiateFlags.NTLMSSP_NEGOTIATE_OEM_WORKSTATION_SUPPLIED)
+	{
+		workstationName = xmalloc(workstationLen);
+		s->p = s->data + workstationBufferOffset;
+		in_uint8a(s, workstationName, workstationLen);
+		printf("Workstation name: ");
+		print_wstr(workstationName, workstationLen);
+		printf("\n");
+	}
+
+	if(domainName != NULL)
+		xfree(domainName);
+
+	if(workstationName != NULL)
+		xfree(workstationName);
+}
+
+void ntlm_recv_challenge_message(STREAM s)
+{
+
+	/* TargetNameFields (8 bytes) */
+	uint16 targetNameLen;
+	uint16 targetNameMaxLen;
+	uint32 targetNameBufferOffset;
+
+	NEGOTIATE negotiateFlags; /* A NEGOTIATE data structure */
+	uint8 serverChallenge[8]; /* 64-byte value containing NTLM challenge */
+
+	/* TargetInfoFields (8 bytes) */
+	uint16 targetInfoLen;
+	uint16 targetInfoMaxLen;
+	uint32 targetInfoBufferOffset;
+
+	/* Payload */
+	char* targetName = NULL;
+	char* targetInfo = NULL;
+
+	in_uint16(s, targetNameLen);
+	in_uint16(s, targetNameMaxLen);
+	in_uint32(s, targetNameBufferOffset);
+
+	ntlm_recv_negotiate_flags(s, &negotiateFlags);
+	in_uint8a(s, serverChallenge, 8);
+	in_uint8s(s, 8); /* Reserved */
+
+	printf("Server Challenge:\n");
+	print_hexdump(serverChallenge, 8);
+
+	in_uint16(s, targetInfoLen);
+	in_uint16(s, targetInfoMaxLen);
+	in_uint32(s, targetInfoBufferOffset);
+
+	/* Version, VERSION data structure (8 bytes) */
+	/* [MS-NLMP] says it should be ignored */
+	in_uint8s(s, 8); /* Skip */
+
+	if(negotiateFlags.NTLMSSP_REQUEST_TARGET)
+	{
+		targetName = xmalloc(targetNameLen);
+		s->p = s->data + targetNameBufferOffset;
+		in_uint8a(s, targetName, targetNameLen);
+		printf("Target name: ");
+		print_wstr(targetName, targetNameLen);
+		printf("\n");
+	}
+
+	if(negotiateFlags.NTLMSSP_NEGOTIATE_TARGET_INFO)
+	{
+		targetInfo = xmalloc(targetInfoLen);
+		s->p = s->data + targetInfoBufferOffset;
+		in_uint8a(s, targetInfo, targetInfoLen);
+		printf("Target info: %s\n", targetInfo);
+	}
+
+	if(targetName != NULL)
+		xfree(targetName);
+
+	if(targetInfo != NULL)
+		xfree(targetInfo);
+}
+
+void ntlm_recv_authenticate_message(STREAM s)
+{
+	/* LmChallengeResponseFields */
+	uint16 lmChallengeResponseLen;
+	uint16 lmChallengeResponseMaxLen;
+	uint32 lmChallengeResponseBufferOffset;
+
+	/* NtChallengeResponseFields */
+	uint16 ntChallengeResponseLen;
+	uint16 ntChallengeResponseMaxLen;
+	uint32 ntChallengeResponseBufferOffset;
+
+	/* DomainNameFields */
+	uint16 domainNameLen;
+	uint16 domainNameMaxLen;
+	uint32 domainNameBufferOffset;
+
+	/* UserNameFields */
+	uint16 userNameLen;
+	uint16 userNameMaxLen;
+	uint32 userNameBufferOffset;
+
+	/* WorkstationFields */
+	uint16 workstationLen;
+	uint16 workstationMaxLen;
+	uint32 workstationBufferOffset;
+
+	/* EncryptedRandomSessionKeyFields */
+	uint16 encryptedRandomSessionKeyLen;
+	uint16 encryptedRandomSessionKeyMaxLen;
+	uint32 encryptedRandomSessionKeyBufferOffset;
+
+	NEGOTIATE negotiateFlags;
+
+	/* Payload */
+	char* lmChallengeResponse = NULL;
+	char* ntChallengeResponse = NULL;
+	char* domainName = NULL;
+	char* userName = NULL;
+	char* workstationName = NULL;
+	char* encryptedRandomSessionKey = NULL;
+
+	in_uint16(s, lmChallengeResponseLen);
+	in_uint16(s, lmChallengeResponseMaxLen);
+	in_uint32(s, lmChallengeResponseBufferOffset);
+
+	in_uint16(s, ntChallengeResponseLen);
+	in_uint16(s, ntChallengeResponseMaxLen);
+	in_uint32(s, ntChallengeResponseBufferOffset);
+
+	in_uint16_le(s, domainNameLen);
+	in_uint16_le(s, domainNameMaxLen);
+	in_uint32_le(s, domainNameBufferOffset);
+
+	in_uint16_le(s, userNameLen);
+	in_uint16_le(s, userNameMaxLen);
+	in_uint32_le(s, userNameBufferOffset);
+
+	in_uint16_le(s, workstationLen);
+	in_uint16_le(s, workstationMaxLen);
+	in_uint16_le(s, workstationBufferOffset);
+
+	in_uint16_le(s, encryptedRandomSessionKeyLen);
+	in_uint16_le(s, encryptedRandomSessionKeyMaxLen);
+	in_uint16_le(s, encryptedRandomSessionKeyBufferOffset);
+
+	ntlm_recv_negotiate_flags(s, &negotiateFlags);
+	in_uint8s(s, 8); /* Version */
+	in_uint8s(s, 16); /* MIC */
+
+	/* Payload */
+
+	if(lmChallengeResponseLen > 0 && lmChallengeResponseMaxLen > 0)
+	{
+		lmChallengeResponse = xmalloc(lmChallengeResponseLen);
+		s->p = s->data + lmChallengeResponseBufferOffset;
+		in_uint8a(s, lmChallengeResponse, lmChallengeResponseLen);
+		printf("lmChallengeResponse: %s\n", lmChallengeResponse);
+	}
+
+	if(ntChallengeResponseLen > 0 && ntChallengeResponseMaxLen > 0)
+	{
+		ntChallengeResponse = xmalloc(ntChallengeResponseLen);
+		s->p = s->data + ntChallengeResponseBufferOffset;
+		in_uint8a(s, ntChallengeResponse, 16);
+		printf("---ntChallengeResponse---\n");
+		print_hexdump(ntChallengeResponse, 16);
+		
+	}
+
+	if(negotiateFlags.NTLMSSP_NEGOTIATE_OEM_DOMAIN_SUPPLIED)
+	{
+		domainName = xmalloc(domainNameLen);
+		s->p = s->data + domainNameBufferOffset;
+		in_uint8a(s, domainName, domainNameLen);
+		printf("Domain name: %s\n", domainName);
+	}
+
+	if(userNameLen > 0 && userNameMaxLen > 0)
+	{
+		userName = xmalloc(userNameLen);
+		s->p = s->data + userNameBufferOffset;
+		in_uint8a(s, userName, userNameLen);
+		printf("User name: ");
+		print_wstr(userName, userNameLen);
+		printf("\n");
+	}
+
+	if(negotiateFlags.NTLMSSP_NEGOTIATE_OEM_WORKSTATION_SUPPLIED)
+	{
+		workstationName = xmalloc(workstationLen);
+		s->p = s->data + workstationBufferOffset;
+		in_uint8a(s, workstationName, workstationLen);
+		printf("Workstation name: %s\n", workstationName);
+	}
+
+	if(encryptedRandomSessionKeyLen > 0 && encryptedRandomSessionKeyMaxLen > 0)
+	{
+		encryptedRandomSessionKey = xmalloc(encryptedRandomSessionKeyLen);
+		s->p = s->data + encryptedRandomSessionKeyBufferOffset;
+		in_uint8a(s, encryptedRandomSessionKey, encryptedRandomSessionKeyLen);
+		printf("encryptedRandomSessionKey: %s\n", encryptedRandomSessionKey);
+	}
+
+	if(lmChallengeResponse != NULL)
+		xfree(lmChallengeResponse);
+
+	if(ntChallengeResponse != NULL)
+		xfree(ntChallengeResponse);
+
+	if(domainName != NULL)
+		xfree(domainName);
+
+	if(userName != NULL)
+		xfree(userName);
+
+	if(workstationName != NULL)
+		xfree(workstationName);
+
+	if(encryptedRandomSessionKey != NULL)
+		xfree(encryptedRandomSessionKey);
+}
 
 void decode_ts_request(char* bytes, int length)
 {
@@ -101,14 +513,24 @@ void decode_ts_request(char* bytes, int length)
 
 		for(i = 0; i < ts_request->negoTokens->list.count; i++)
 		{
+			STREAM s = xmalloc(sizeof(struct stream));
 			nego_token = 0;
+			
 			rval = asn_DEF_NegotiationToken.ber_decoder(0, &asn_DEF_NegotiationToken, (void **)&nego_token,
 				ts_request->negoTokens->list.array[i]->negoToken.buf,
 				ts_request->negoTokens->list.array[i]->negoToken.size, 0);
 
 			printf("----------------------NegotiationToken-------------------\n");
-			hexdump(ts_request->negoTokens->list.array[i]->negoToken.buf, ts_request->negoTokens->list.array[i]->negoToken.size);
+			print_hexdump(ts_request->negoTokens->list.array[i]->negoToken.buf, ts_request->negoTokens->list.array[i]->negoToken.size);
 			printf("---------------------------------------------------------\n");
+
+			s->data = (unsigned char*)(ts_request->negoTokens->list.array[i]->negoToken.buf);
+			s->size = ts_request->negoTokens->list.array[i]->negoToken.size;
+			s->p = s->data;
+			s->end = s->p + s->size;
+
+			ntlmssp_recv(s);
+			xfree(s);
 		}
 	}
 	else
@@ -118,7 +540,7 @@ void decode_ts_request(char* bytes, int length)
 	}
 }
 
-void hexdump(char* bytes, int length)
+void print_hexdump(char* bytes, int length)
 {
 	int i;
 	int j;
@@ -162,6 +584,18 @@ void hexdump(char* bytes, int length)
 	}
 }
 
+void print_wstr(char* bytes, int length)
+{
+	int i;
+	unsigned char b;
+
+	for(i = 0; i < length; i += 2)
+	{
+		b = (unsigned char)bytes[i];
+		printf("%c", b);
+	}
+}
+
 int tls_read_record(SSL* ssl, char* b, int size)
 {
 	int bytesRead = 0;
@@ -172,7 +606,7 @@ int tls_read_record(SSL* ssl, char* b, int size)
 	{
 		case SSL_ERROR_NONE:
 			printf("read %d bytes\n", n);
-			hexdump(b, n);
+			print_hexdump(b, n);
 			bytesRead += n;
 			break;
 
@@ -230,8 +664,8 @@ int tls_write_record(SSL* ssl, char* b, int size)
 	switch(SSL_get_error(ssl, n))
 	{
 		case SSL_ERROR_NONE:
-			printf("Wrote %d bytes\n", n);
-			hexdump(b, n);
+			//printf("Wrote %d bytes\n", n);
+			//print_hexdump(b, n);
 			bytesWritten += n;
 			break;
 
@@ -337,12 +771,12 @@ int main(int argc, char* argv[])
 	/* Receive client initial */
 	n = recv(srcfd, buffer, BUFFER_SIZE, 0);
 	printf("Received RDP client initial from source\n");
-	hexdump(buffer, n);
+	print_hexdump(buffer, n);
 
 	/* Send server initial */
 	n = send(srcfd, serverInitial, sizeof(serverInitial) - 1, 0);
 	printf("Sent RDP server initial to source\n");
-	hexdump(serverInitial, n);
+	print_hexdump(serverInitial, n);
 
 	/* Initialize OpenSSL */
 	SSL_load_error_strings();
@@ -421,12 +855,12 @@ int main(int argc, char* argv[])
 	/* Send client initial */
 	n = send(dstfd, clientInitial, sizeof(clientInitial) - 1, 0);
 	printf("Sent RDP client initial to destination\n");
-	hexdump(buffer, n);
+	print_hexdump(buffer, n);
 
 	/* Receive server initial */
 	n = recv(dstfd, buffer, BUFFER_SIZE, 0);
 	printf("Received RDP server initial from destination\n");
-	hexdump(buffer, n);
+	print_hexdump(buffer, n);
 
 	/* Create destination TLS context */
 	ctxDst = SSL_CTX_new(TLSv1_client_method());
@@ -537,7 +971,7 @@ int main(int argc, char* argv[])
 		{
 			printf("srcfd writable\n");
 			tls_write_record(sslSrc, dstBuffer, inDstBuffer);
-			inDstBuffer = 0;	
+			inDstBuffer = 0;
 		}
 		if(FD_ISSET(dstfd, &writefds) && inSrcBuffer > 0)
 		{
