df2ba01f5778286dc2f03735be9905a365a97e89
Jay Sorg <jay.sorg@gmail.com>
2010-01-03 23:27:57 +0000

    work on rdpsnd
    git-svn-id: https://freerdp.svn.sourceforge.net/svnroot/freerdp/branches/channel-jay1@260 7ffbcff5-ea86-4e99-a76d-b9f7f6476058

--- channels/rdpsnd/rdpsnd_main.c
+++ channels/rdpsnd/rdpsnd_main.c
@@ -8,6 +8,37 @@
 #include <sys/un.h>
 #include "types_ui.h"
 #include "vchan.h"
+#include "chan_arch.h"
+
+#define SNDC_CLOSE         1
+#define SNDC_WAVE          2
+#define SNDC_SETVOLUME     3
+#define SNDC_SETPITCH      4
+#define SNDC_WAVECONFIRM   5
+#define SNDC_TRAINING      6
+#define SNDC_FORMATS       7
+#define SNDC_CRYPTKEY      8
+#define SNDC_WAVEENCRYPT   9
+#define SNDC_UDPWAVE       10
+#define SNDC_UDPWAVELAST   11
+#define SNDC_QUALITYMODE   12
+
+#define TSSNDCAPS_ALIVE  1
+#define TSSNDCAPS_VOLUME 2
+#define TSSNDCAPS_PITCH  4
+
+struct _RD_WAVEFORMATEX
+{
+	uint16 wFormatTag;
+	uint16 nChannels;
+	uint32 nSamplesPerSec;
+	uint32 nAvgBytesPerSec;
+	uint16 nBlockAlign;
+	uint16 wBitsPerSample;
+	uint16 cbSize;
+};
+typedef struct _RD_WAVEFORMATEX RD_WAVEFORMATEX;
+
 
 struct wait_obj
 {
@@ -34,7 +65,10 @@ static struct wait_obj g_data_in_event;
 static struct wait_obj g_thread_done_event;
 static struct data_in_item * volatile g_list_head;
 static struct data_in_item * volatile g_list_tail;
+/* for locking the linked list */
 static pthread_mutex_t * g_mutex;
+
+static int g_cBlockNo;
 
 static int
 init_wait_obj(struct wait_obj * obj, const char * name)
@@ -157,7 +191,7 @@ wait(int timeout, int numr, int * listr)
 }
 
 /* called by main thread
-   add item to linked list and infor worker thread that there is data */
+   add item to linked list and inform worker thread that there is data */
 static void
 signal_data_in(void)
 {
@@ -166,7 +200,9 @@ signal_data_in(void)
 	item = (struct data_in_item *) malloc(sizeof(struct data_in_item));
 	item->next = 0;
 	item->data = g_data_in[0];
+	g_data_in[0] = 0;
 	item->data_size = g_data_in_size[0];
+	g_data_in_size[0] = 0;
 	pthread_mutex_lock(g_mutex);
 	if (g_list_tail == 0)
 	{
@@ -183,9 +219,185 @@ signal_data_in(void)
 }
 
 static int
+thread_process_message_formats(char * data, int data_size)
+{
+	int index;
+	int format_count;
+	int wf_size;
+	int out_format_count;
+	int size;
+	char * ldata;
+	char * out_data;
+	RD_WAVEFORMATEX * formats;
+	RD_WAVEFORMATEX wf;
+	uint32 error;
+
+	/* skip:
+		dwFlags (4 bytes),
+		dwVolume (4 bytes),
+		dwPitch (4 bytes),
+		wDGramPort (2 bytes) */
+	format_count = GET_UINT16(data, 14); /* wNumberOfFormats */
+	if ((format_count < 1) || (format_count > 1000))
+	{
+		printf("thread_process_message_formats: bad format_count %d\n",
+			format_count);
+		return 1;
+	}
+	g_cBlockNo = GET_UINT8(data, 16); /* cLastBlockConfirmed */
+	/* skip:
+		wVersion (2 bytes)
+		bPad (1 byte) */
+	/* remainder is sndFormats (variable) */
+	ldata = data + 20;
+	out_format_count = 0;
+	wf_size = 18; /* packed size of wf */
+	size = sizeof(RD_WAVEFORMATEX) * format_count;
+	formats = (RD_WAVEFORMATEX *) malloc(size);
+	memset(&wf, 0, sizeof(wf));
+	for (index = 0; index < format_count; index++)
+	{
+		if ((ldata + wf_size) > (data + data_size))
+		{
+			printf("thread_process_message_formats: error\n");
+			break;
+		}
+		wf.wFormatTag = GET_UINT16(ldata, 0);
+		wf.nChannels = GET_UINT16(ldata, 2);
+		wf.nSamplesPerSec = GET_UINT32(ldata, 4);
+		wf.nAvgBytesPerSec = GET_UINT32(ldata, 8);
+		wf.nBlockAlign = GET_UINT16(ldata, 12);
+		wf.wBitsPerSample = GET_UINT16(ldata, 14);
+		wf.cbSize = GET_UINT16(ldata, 16);
+		ldata += wf.cbSize;
+		ldata += wf_size;
+		//if (alsa_format_supported(&wf))
+		if (index == 0)
+		{
+			formats[out_format_count] = wf;
+			out_format_count++;
+		}
+	}
+	size = 24 + out_format_count * wf_size;
+	out_data = (char *) malloc(size + 16);
+	SET_UINT8(out_data, 0, SNDC_FORMATS); /* Header (4 bytes) */
+	SET_UINT8(out_data, 1, 0);
+	SET_UINT16(out_data, 2, size - 4);
+	SET_UINT32(out_data, 4, TSSNDCAPS_ALIVE | TSSNDCAPS_VOLUME); /* dwFlags */
+	SET_UINT32(out_data, 8, 0xffffffff); /* dwVolume */
+	SET_UINT32(out_data, 12, 0); /* dwPitch */
+	SET_UINT16(out_data, 16, 0); /* wDGramPort */
+	SET_UINT16(out_data, 18, out_format_count); /* wNumberOfFormats */
+	SET_UINT8(out_data, 20, 0); /* padding */
+	SET_UINT16(out_data, 21, 2); /* version */
+	SET_UINT8(out_data, 23, 0); /* padding */
+	ldata = out_data + 24;
+	for (index = 0; index < out_format_count; index++)
+	{
+		wf = formats[index];
+		SET_UINT16(ldata, 0, wf.wFormatTag);
+		SET_UINT16(ldata, 2, wf.nChannels);
+		SET_UINT32(ldata, 4, wf.nSamplesPerSec);
+		SET_UINT32(ldata, 8, wf.nAvgBytesPerSec);
+		SET_UINT16(ldata, 12, wf.nBlockAlign);
+		SET_UINT16(ldata, 14, wf.wBitsPerSample);
+		SET_UINT16(ldata, 16, wf.cbSize);
+		ldata += wf.cbSize;
+		ldata += wf_size;
+	}
+	free(formats);
+	error = g_ep.pVirtualChannelWrite(g_open_handle[0],
+		out_data, size, out_data);
+	if (error != CHANNEL_RC_OK)
+	{
+		printf("thread_process_message_formats: VirtualChannelWrite "
+			"failed %d\n", error);
+		return 1;
+	}
+	return 0;
+}
+
+static int
+thread_process_message_training(char * data, int data_size)
+{
+	int wTimeStamp;
+	int wPackSize;
+	int size;
+	char * out_data;
+	uint32 error;
+
+	wTimeStamp = GET_UINT16(data, 0);
+	wPackSize = GET_UINT16(data, 2);
+	if (wPackSize != data_size - 4)
+	{
+		printf("thread_process_message_training: size error\n");
+	}
+	size = 8;
+	out_data = (char *) malloc(size);
+	SET_UINT16(out_data, 0, SNDC_TRAINING | 0x2300);
+	SET_UINT16(out_data, 2, size - 4);
+	SET_UINT16(out_data, 4, wTimeStamp);
+	SET_UINT16(out_data, 6, 0);
+	error = g_ep.pVirtualChannelWrite(g_open_handle[0],
+		out_data, size, out_data);
+	if (error != CHANNEL_RC_OK)
+	{
+		printf("thread_process_message_training: VirtualChannelWrite "
+			"failed %d\n", error);
+		return 1;
+	}
+	return 0;
+}
+
+static int
+thread_process_message_wave(char * data, int data_size)
+{
+	return 0;
+}
+
+static int
+thread_process_message_close(char * data, int data_size)
+{
+	return 0;
+}
+
+static int
+thread_process_message_setvolume(char * data, int data_size)
+{
+	return 0;
+}
+
+static int
 thread_process_message(char * data, int data_size)
 {
-	printf("thread_process_message: data_size %d\n", data_size);
+	int opcode;
+	int size;
+
+	opcode = GET_UINT8(data, 0);
+	size = GET_UINT16(data, 2);
+	printf("thread_process_message: data_size %d opcode %d size %d\n",
+		data_size, opcode, size);
+	switch (opcode)
+	{
+		case SNDC_FORMATS:
+			thread_process_message_formats(data + 4, size);
+			break;
+		case SNDC_TRAINING:
+			thread_process_message_training(data + 4, size);
+			break;
+		case SNDC_WAVE:
+			thread_process_message_wave(data + 4, size);
+			break;
+		case SNDC_CLOSE:
+			thread_process_message_close(data + 4, size);
+			break;
+		case SNDC_SETVOLUME:
+			thread_process_message_setvolume(data + 4, size);
+			break;
+		default:
+			printf("thread_process_message: unknown opcode\n");
+			break;
+	}
 	return 0;
 }
 
@@ -301,6 +513,9 @@ OpenEvent(uint32 openHandle, uint32 event, void * pData, uint32 dataLength,
 		case CHANNEL_EVENT_DATA_RECEIVED:
 			OpenEventProcessReceived(openHandle, pData, dataLength,
 				totalLength, dataFlags);
+			break;
+		case CHANNEL_EVENT_WRITE_COMPLETE:
+			free(pData);
 			break;
 	}
 }
