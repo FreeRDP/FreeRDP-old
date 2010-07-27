30fb51a9628f72b6213b3d9ec8d200949cf2b8a3
Mads Kiilerich <mads@kiilerich.com>
2010-07-27 02:49:36 +0200

    crypto: Separate crypto_simple.c (the built-in crypto) and crypto_openssl.c
    Introduce crypto_openssl.c and crypto_simple.c and related crypto libraries and
    let configure/make files choose the right one.
    
    Unused traces of polarssl are removed.
    
    --HG--
    rename : libfreerdp/crypto.c => libfreerdp/crypto_openssl.c
    rename : libfreerdp/crypto.c => libfreerdp/crypto_simple.c

--- libfreerdp/crypto_openssl.c
+++ libfreerdp/crypto_openssl.c
@@ -1,6 +1,6 @@
 /* -*- c-basic-offset: 8 -*-
    FreeRDP: A Remote Desktop Protocol client.
-   Cryptographic Abstraction Layer
+   OpenSSL Cryptographic Abstraction Layer
 
    Copyright (C) Marc-Andre Moreau <marcandre.moreau@gmail.com> 2010
 
@@ -23,14 +23,6 @@
 #include "crypto.h"
 #include "mem.h"
 
-#ifdef DISABLE_TLS
-	#undef CRYPTO_OPENSSL
-#else
-	#define CRYPTO_OPENSSL
-#endif
-
-#ifdef CRYPTO_OPENSSL
-
 #include <openssl/ssl.h>
 #include <openssl/err.h>
 #include <openssl/rc4.h>
@@ -50,141 +42,70 @@ struct crypto_sha1_struct
 	SHA_CTX sha_ctx;
 };
 
-struct crypto_md5_struct
-{
-	MD5_CTX md5_ctx;
-};
-
-struct crypto_rc4_struct {
-	RC4_KEY rc4_key;
-};
-
-struct crypto_cert_struct
-{
-	X509 * px509;
-};
-
-struct crypto_public_key_struct
-{
-	RSA * prsa;
-};
-
-#else /* built-in crypto */
-
-#include "ssl.h"
-
-struct crypto_sha1_struct
-{
-	struct sha1_context data;
-};
-
-struct crypto_md5_struct
-{
-	struct md5_context data;
-};
-
-struct crypto_rc4_struct {
-	struct rc4_state data;
-};
-
-struct crypto_cert_struct
-{
-	char * data;
-};
-
-struct crypto_public_key_struct
-{
-	char * data;
-};
-
-#endif
-
 CryptoSha1
 crypto_sha1_init(void)
 {
 	CryptoSha1 sha1 = xmalloc(sizeof(*sha1));
-#ifdef CRYPTO_OPENSSL
 	SHA1_Init(&sha1->sha_ctx);
-#else /* built-in crypto */
-	ssl_sha1_init(&sha1->data);
-#endif
 	return sha1;
 }
 
 void
 crypto_sha1_update(CryptoSha1 sha1, uint8 * data, uint32 len)
 {
-#ifdef CRYPTO_OPENSSL
 	SHA1_Update(&sha1->sha_ctx, data, len);
-#else /* built-in crypto */
-	ssl_sha1_update(&sha1->data, data, len);
-#endif
 }
 
 void
 crypto_sha1_final(CryptoSha1 sha1, uint8 * out_data)
 {
-#ifdef CRYPTO_OPENSSL
 	SHA1_Final(out_data, &sha1->sha_ctx);
-#else /* built-in crypto */
-	ssl_sha1_final(&sha1->data, out_data);
-#endif	
 	xfree(sha1);
 }
+
+struct crypto_md5_struct
+{
+	MD5_CTX md5_ctx;
+};
 
 CryptoMd5
 crypto_md5_init(void)
 {
 	CryptoMd5 md5 = xmalloc(sizeof(*md5));
-#ifdef CRYPTO_OPENSSL
 	MD5_Init(&md5->md5_ctx);
-#else /* built-in crypto */
-	ssl_md5_init(&md5->data);
-#endif
 	return md5;
 }
 
 void
 crypto_md5_update(CryptoMd5 md5, uint8 * data, uint32 len)
 {
-#ifdef CRYPTO_OPENSSL
 	MD5_Update(&md5->md5_ctx, data, len);
-#else /* built-in crypto */
-	ssl_md5_update(&md5->data, data, len);
-#endif
 }
 
 void
 crypto_md5_final(CryptoMd5 md5, uint8 * out_data)
 {
-#ifdef CRYPTO_OPENSSL
 	MD5_Final(out_data, &md5->md5_ctx);
-#else /* built-in crypto */
-	ssl_md5_final(&md5->data, out_data);
-#endif
 	xfree(md5);
 }
+
+struct crypto_rc4_struct
+{
+	RC4_KEY rc4_key;
+};
 
 CryptoRc4
 crypto_rc4_init(uint8 * key, uint32 len)
 {
 	CryptoRc4 rc4 = xmalloc(sizeof(*rc4));
-#ifdef CRYPTO_OPENSSL
 	RC4_set_key(&rc4->rc4_key, len, key);
-#else /* built-in crypto */
-	ssl_rc4_set_key(&rc4->data, key, len);
-#endif
 	return rc4;
 }
 
 void
 crypto_rc4(CryptoRc4 rc4, uint32 len, uint8 * in_data, uint8 * out_data)
 {
-#ifdef CRYPTO_OPENSSL
 	RC4(&rc4->rc4_key, len, in_data, out_data);
-#else /* built-in crypto */
-	ssl_rc4_crypt(&rc4->data, in_data, out_data, len);
-#endif
 }
 
 void
@@ -193,7 +114,6 @@ crypto_rc4_free(CryptoRc4 rc4)
 	xfree(rc4);
 }
 
-#ifdef CRYPTO_OPENSSL
 static void
 reverse(uint8 * p, int len)
 {
@@ -207,13 +127,10 @@ reverse(uint8 * p, int len)
 		p[j] = temp;
 	}
 }
-#endif
 
 void
 crypto_rsa_encrypt(int len, uint8 * in, uint8 * out, uint32 modulus_size, uint8 * modulus, uint8 * exponent)
 {
-#ifdef CRYPTO_OPENSSL
-
 	BN_CTX *ctx;
 	BIGNUM mod, exp, x, y;
 	uint8 inr[SEC_MAX_MODULUS_SIZE];
@@ -244,48 +161,51 @@ crypto_rsa_encrypt(int len, uint8 * in, uint8 * out, uint32 modulus_size, uint8 
 	BN_free(&exp);
 	BN_free(&mod);
 	BN_CTX_free(ctx);
-	
-#else /* built-in crypto */
-
-	ssl_rsa_encrypt(out, in, len, modulus_size, modulus, exponent);
-	
-#endif	
 }
+
+struct crypto_cert_struct
+{
+	X509 * px509;
+};
 
 CryptoCert
 crypto_cert_read(uint8 * data, uint32 len)
 {
 	CryptoCert cert = xmalloc(sizeof(*cert));
-#ifdef CRYPTO_OPENSSL
-	
 	/* this will move the data pointer but we don't care, we don't use it again */
 	cert->px509 = d2i_X509(NULL, (D2I_X509_CONST unsigned char **) &data, len);
-	
-#else /* built-in crypto */
-
-	cert->data = ssl_cert_read(data, len);
-
-#endif
 	return cert;
 }
 
 void
 crypto_cert_free(CryptoCert cert)
 {
-#ifdef CRYPTO_OPENSSL
 	X509_free(cert->px509);
-#else /* built-in crypto */
-	ssl_cert_free(cert->data);
-#endif
     xfree(cert);
 }
+
+RD_BOOL
+crypto_cert_verify(CryptoCert server_cert, CryptoCert cacert)
+{
+	/* FIXME: do the actual verification */
+	return True;
+}
+
+int
+crypto_cert_print_fp(FILE * fp, CryptoCert cert)
+{
+	return X509_print_fp(fp, cert->px509);
+}
+
+struct crypto_public_key_struct
+{
+	RSA * prsa;
+};
 
 CryptoPublicKey
 crypto_cert_get_public_key(CryptoCert cert, uint32 * key_len)
 {
 	CryptoPublicKey public_key = xmalloc(sizeof(*public_key));
-#ifdef CRYPTO_OPENSSL
-	
 	int nid;
 	EVP_PKEY *epk = NULL;
 
@@ -311,53 +231,19 @@ crypto_cert_get_public_key(CryptoCert cert, uint32 * key_len)
 	public_key->prsa = RSAPublicKey_dup((RSA *) epk->pkey.ptr);
 	*key_len = RSA_size(public_key->prsa);
 	EVP_PKEY_free(epk);
-	
-#else /* built-in crypto */
-
-	public_key->data = ssl_cert_get_public_key(cert->data, key_len);
-	
-#endif
 	return public_key;
-}
-
-RD_BOOL
-crypto_cert_verify(CryptoCert server_cert, CryptoCert cacert)
-{
-	/* FIXME: do the actual verification */
-	
-#ifdef CRYPTO_OPENSSL
-	return True;
-#else /* built-in crypto */
-	return ssl_cert_verify(server_cert->data, cacert->data);
-#endif
-}
-
-int
-crypto_cert_print_fp(FILE * fp, CryptoCert cert)
-{
-#ifdef CRYPTO_OPENSSL
-	return X509_print_fp(fp, cert->px509);
-#else /* built-in crypto */
-	return ssl_cert_print_fp(fp, cert->data);
-#endif
 }
 
 void
 crypto_public_key_free(CryptoPublicKey public_key)
 {
-#ifdef CRYPTO_OPENSSL
 	RSA_free(public_key->prsa);
-#else /* built-in crypto */
-	ssl_public_key_free(public_key->data);
-#endif
 	xfree(public_key);
 }
 
 int
 crypto_public_key_get_exp_mod(CryptoPublicKey public_key, uint8 * exponent, uint32 max_exp_len, uint8 * modulus, uint32 max_mod_len)
 {
-#ifdef CRYPTO_OPENSSL
-	
 	int len;
 
 	if ((BN_num_bytes(public_key->prsa->e) > (int) max_exp_len) || (BN_num_bytes(public_key->prsa->n) > (int) max_mod_len))
@@ -370,10 +256,4 @@ crypto_public_key_get_exp_mod(CryptoPublicKey public_key, uint8 * exponent, uint
 	reverse(modulus, len);
 	
 	return 0;
-	
-#else /* built-in crypto */
-
-	return ssl_public_key_get_exp_mod(public_key->data, exponent, max_exp_len, modulus, max_mod_len);
-	
-#endif
 }
---
+++ libfreerdp/crypto_simple.c
@@ -0,0 +1,169 @@
+/* -*- c-basic-offset: 8 -*-
+   FreeRDP: A Remote Desktop Protocol client.
+   Simple Cryptographic Abstraction Layer
+
+   Copyright (C) Marc-Andre Moreau <marcandre.moreau@gmail.com> 2010
+
+   This program is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; either version 2 of the License, or
+   (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program; if not, write to the Free Software
+   Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+*/
+
+#include "frdp.h"
+#include "crypto.h"
+#include "mem.h"
+
+/* TODO: Merge with ssl.c */
+#include "ssl.h"
+
+struct crypto_sha1_struct
+{
+	struct sha1_context data;
+};
+
+CryptoSha1
+crypto_sha1_init(void)
+{
+	CryptoSha1 sha1 = xmalloc(sizeof(*sha1));
+	ssl_sha1_init(&sha1->data);
+	return sha1;
+}
+
+void
+crypto_sha1_update(CryptoSha1 sha1, uint8 * data, uint32 len)
+{
+	ssl_sha1_update(&sha1->data, data, len);
+}
+
+void
+crypto_sha1_final(CryptoSha1 sha1, uint8 * out_data)
+{
+	ssl_sha1_final(&sha1->data, out_data);
+	xfree(sha1);
+}
+
+struct crypto_md5_struct
+{
+	struct md5_context data;
+};
+
+CryptoMd5
+crypto_md5_init(void)
+{
+	CryptoMd5 md5 = xmalloc(sizeof(*md5));
+	ssl_md5_init(&md5->data);
+	return md5;
+}
+
+void
+crypto_md5_update(CryptoMd5 md5, uint8 * data, uint32 len)
+{
+	ssl_md5_update(&md5->data, data, len);
+}
+
+void
+crypto_md5_final(CryptoMd5 md5, uint8 * out_data)
+{
+	ssl_md5_final(&md5->data, out_data);
+	xfree(md5);
+}
+
+struct crypto_rc4_struct
+{
+	struct rc4_state data;
+};
+
+CryptoRc4
+crypto_rc4_init(uint8 * key, uint32 len)
+{
+	CryptoRc4 rc4 = xmalloc(sizeof(*rc4));
+	ssl_rc4_set_key(&rc4->data, key, len);
+	return rc4;
+}
+
+void
+crypto_rc4(CryptoRc4 rc4, uint32 len, uint8 * in_data, uint8 * out_data)
+{
+	ssl_rc4_crypt(&rc4->data, in_data, out_data, len);
+}
+
+void
+crypto_rc4_free(CryptoRc4 rc4)
+{
+	xfree(rc4);
+}
+
+void
+crypto_rsa_encrypt(int len, uint8 * in, uint8 * out, uint32 modulus_size, uint8 * modulus, uint8 * exponent)
+{
+	ssl_rsa_encrypt(out, in, len, modulus_size, modulus, exponent);
+}
+
+struct crypto_cert_struct
+{
+	char * data;
+};
+
+CryptoCert
+crypto_cert_read(uint8 * data, uint32 len)
+{
+	CryptoCert cert = xmalloc(sizeof(*cert));
+	cert->data = ssl_cert_read(data, len);
+	return cert;
+}
+
+void
+crypto_cert_free(CryptoCert cert)
+{
+	ssl_cert_free(cert->data);
+    xfree(cert);
+}
+
+RD_BOOL
+crypto_cert_verify(CryptoCert server_cert, CryptoCert cacert)
+{
+	/* FIXME: do the actual verification */
+	return ssl_cert_verify(server_cert->data, cacert->data);
+}
+
+int
+crypto_cert_print_fp(FILE * fp, CryptoCert cert)
+{
+	return ssl_cert_print_fp(fp, cert->data);
+}
+
+struct crypto_public_key_struct
+{
+	char * data;
+};
+
+CryptoPublicKey
+crypto_cert_get_public_key(CryptoCert cert, uint32 * key_len)
+{
+	CryptoPublicKey public_key = xmalloc(sizeof(*public_key));
+	public_key->data = ssl_cert_get_public_key(cert->data, key_len);
+	return public_key;
+}
+
+void
+crypto_public_key_free(CryptoPublicKey public_key)
+{
+	ssl_public_key_free(public_key->data);
+	xfree(public_key);
+}
+
+int
+crypto_public_key_get_exp_mod(CryptoPublicKey public_key, uint8 * exponent, uint32 max_exp_len, uint8 * modulus, uint32 max_mod_len)
+{
+	return ssl_public_key_get_exp_mod(public_key->data, exponent, max_exp_len, modulus, max_mod_len);
+}
