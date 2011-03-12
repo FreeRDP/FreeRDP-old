84a5af1e9f0e0c0ea3d97214fdd72a978f5acc14
Jay Sorg <jay.sorg@gmail.com>
2011-03-11 20:57:19 -0800

    update process_fp from docs

--- libfreerdp/rdp.c
+++ libfreerdp/rdp.c
@@ -1541,16 +1541,20 @@ process_redirect_pdu(rdpRdp * rdp, STREAM s)
 	in_uint8s(s, length - (s->p - s_start));
 }
 
+/* process fast path */
 static void
 process_fp(rdpRdp * rdp, STREAM s)
 {
-	uint16 count, x, y;
-	uint8 type, ctype;
-	uint8 *next;
+	int numberOrders;
+	int x;
+	int y;
+	int type;
+	int ctype;
 	int frag_bits;
 	int comp_bits;
 	int length;
 	int skip_switch;
+	uint8 *next;
 	uint32 roff, rlen;
 	STREAM ns = &(rdp->mppc_dict.ns);
 	STREAM ts;
@@ -1564,7 +1568,7 @@ process_fp(rdpRdp * rdp, STREAM s)
 		frag_bits = (type & 0x30) >> 4;
 		comp_bits = (type & 0xc0) >> 6;
 		type = type & 0xf;
-		if (comp_bits & 0x2) /* FASTPATH_OUTPUT_COMPRESSION_USED */
+		if (comp_bits & FASTPATH_OUTPUT_COMPRESSION_USED)
 		{
 			in_uint8(s, ctype);
 			in_uint16_le(s, length);
@@ -1603,18 +1607,18 @@ process_fp(rdpRdp * rdp, STREAM s)
 			fd_s = rdp->fragment_data;
 			switch (frag_bits)
 			{
-				case 1: /* FASTPATH_FRAGMENT_LAST */
+				case FASTPATH_FRAGMENT_LAST:
 					out_uint8a(fd_s, ts->p, length);
 					fd_s->end = fd_s->p;
 					fd_s->p = fd_s->data;
 					ts = fd_s;
 					break;
-				case 2: /* FASTPATH_FRAGMENT_FIRST */
+				case FASTPATH_FRAGMENT_FIRST:
 					skip_switch = 1;
 					fd_s->p = fd_s->data;
 					out_uint8a(fd_s, ts->p, length);
 					break;
-				case 3: /* FASTPATH_FRAGMENT_NEXT */
+				case FASTPATH_FRAGMENT_NEXT:
 					skip_switch = 1;
 					out_uint8a(fd_s, ts->p, length);
 					break;
@@ -1627,51 +1631,53 @@ process_fp(rdpRdp * rdp, STREAM s)
 		}
 		switch (type)
 		{
-			case 0: /* update orders */
-				in_uint16_le(ts, count);
-				process_orders(rdp->orders, ts, count);
+			case FASTPATH_UPDATETYPE_ORDERS:
+				in_uint16_le(ts, numberOrders);
+				process_orders(rdp->orders, ts, numberOrders);
 				break;
-			case 1: /* update bitmap */
-				in_uint8s(ts, 2); /* part length */
+			case FASTPATH_UPDATETYPE_BITMAP:
+				in_uint8s(ts, 2);
 				process_bitmap_updates(rdp, ts);
 				break;
-			case 2: /* update palette */
-				in_uint8s(ts, 2); /* uint16 = 2 */
+			case FASTPATH_UPDATETYPE_PALETTE:
+				in_uint8s(ts, 2);
 				process_palette(rdp, ts);
 				break;
-			case 3: /* update synchronize */
+			case FASTPATH_UPDATETYPE_SYNCHRONIZE:
 				break;
-			case 4: /* surface commands */
+			case FASTPATH_UPDATETYPE_SURFCMDS:
 				if (rdp->settings->ui_decode_flags & 3)
 				{
-					ui_decode(rdp->inst, ts->p, (int) (ts->end - ts->p));
+					length = (int) (ts->end - ts->p);
+					if (ui_decode(rdp->inst, ts->p, length) == 0)
+					{
+						break;
+					}
 				}
-				else
-				{
-					surface_cmd(rdp, ts);
-				}
+				surface_cmd(rdp, ts);
 				break;
-			case 5: /* null pointer */
+			case FASTPATH_UPDATETYPE_PTR_NULL:
 				ui_set_null_cursor(rdp->inst);
 				break;
-			case 6: /* default pointer */
+			case FASTPATH_UPDATETYPE_PTR_DEFAULT:
 				break;
-			case 8: /* pointer position */
+			case FASTPATH_UPDATETYPE_PTR_POSITION:
 				in_uint16_le(ts, x);
 				in_uint16_le(ts, y);
 				ui_move_pointer(rdp->inst, x, y);
 				break;
-			case 9: /* color pointer */
+			case FASTPATH_UPDATETYPE_COLOR:
 				process_color_pointer_pdu(rdp, ts);
 				break;
-			case 10:  /* cached pointer */
+			case FASTPATH_UPDATETYPE_CACHED:
 				process_cached_pointer_pdu(rdp, ts);
 				break;
-			case 11:
+			case FASTPATH_UPDATETYPE_POINTER:
 				process_new_pointer_pdu(rdp, ts);
 				break;
 			default:
-				ui_unimpl(rdp->inst, "RDP5 opcode %d\n", type);
+				ui_unimpl(rdp->inst, "fastpath opcode %d\n", type);
+				break;
 		}
 		s->p = next;
 	}
