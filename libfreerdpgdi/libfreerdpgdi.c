d0ef5d63696ab753299765094403564f0b9ef18a
Marc-André Moreau <marcandre.moreau@gmail.com>
2011-03-12 12:08:17 -0500

    documented libfreerdpgdi.c with doxygen

--- libfreerdpgdi/libfreerdpgdi.c
+++ libfreerdpgdi/libfreerdpgdi.c
@@ -29,6 +29,12 @@
 #include "gdi_32bpp.h"
 #include "gdi_16bpp.h"
 
+/**
+ * Get the current device context (a new one is created each time).
+ * http://msdn.microsoft.com/en-us/library/dd144871/
+ * @return current device context
+ */
+
 HDC GetDC()
 {
 	HDC hDC = (HDC) malloc(sizeof(DC));
@@ -41,6 +47,13 @@ HDC GetDC()
 	return hDC;
 }
 
+/**
+ * Create a new device context compatible with the given device context.
+ * http://msdn.microsoft.com/en-us/library/dd183489/
+ * @param hdc device context
+ * @return new compatible device context
+ */
+
 HDC CreateCompatibleDC(HDC hdc)
 {
 	HDC hDC = (HDC) malloc(sizeof(DC));
@@ -52,6 +65,16 @@ HDC CreateCompatibleDC(HDC hdc)
 	hDC->hwnd = NULL;
 	return hDC;
 }
+
+/**
+ * Create a new bitmap with the given width, height, color format and pixel buffer.
+ * http://msdn.microsoft.com/en-us/library/dd183485/
+ * @param nWidth width
+ * @param nHeight height
+ * @param cBitsPerPixel bits per pixel
+ * @param data pixel buffer
+ * @return new bitmap
+ */
 
 HBITMAP CreateBitmap(int nWidth, int nHeight, int cBitsPerPixel, char* data)
 {
@@ -66,6 +89,15 @@ HBITMAP CreateBitmap(int nWidth, int nHeight, int cBitsPerPixel, char* data)
 	return hBitmap;
 }
 
+/**
+ * Create a new bitmap of the given width and height compatible with the current device context.
+ * http://msdn.microsoft.com/en-us/library/dd183488/
+ * @param hdc device context
+ * @param nWidth width
+ * @param nHeight height
+ * @return new bitmap
+ */
+
 HBITMAP CreateCompatibleBitmap(HDC hdc, int nWidth, int nHeight)
 {
 	HBITMAP hBitmap = (HBITMAP) malloc(sizeof(BITMAP));
@@ -78,6 +110,13 @@ HBITMAP CreateCompatibleBitmap(HDC hdc, int nWidth, int nHeight)
 	hBitmap->scanline = nWidth * hBitmap->bytesPerPixel;
 	return hBitmap;
 }
+
+/**
+ * Compare two bitmaps for equality.
+ * @param hBmp1 first bitmap
+ * @param hBmp2 second bitmap
+ * @return 1 if both bitmaps are equal, 0 otherwise
+ */
 
 int CompareBitmaps(HBITMAP hBmp1, HBITMAP hBmp2)
 {
@@ -142,6 +181,15 @@ int CompareBitmaps(HBITMAP hBmp1, HBITMAP hBmp2)
 	return 1;
 }
 
+/**
+ * Create a new pen
+ * http://msdn.microsoft.com/en-us/library/dd183509/.
+ * @param fnPenStyle pen style
+ * @param nWidth pen width
+ * @param crColor pen color
+ * @return new pen
+ */
+
 HPEN CreatePen(int fnPenStyle, int nWidth, int crColor)
 {
 	HPEN hPen = (HPEN) malloc(sizeof(PEN));
@@ -152,6 +200,13 @@ HPEN CreatePen(int fnPenStyle, int nWidth, int crColor)
 	return hPen;
 }
 
+/**
+ * Create a new palette.
+ * http://msdn.microsoft.com/en-us/library/dd183507/
+ * @param lplgpl logical palette
+ * @return new palette
+ */
+
 HPALETTE CreatePalette(LOGPALETTE *lplgpl)
 {
 	HPALETTE hPalette = (HPALETTE) malloc(sizeof(PALETTE));
@@ -159,6 +214,13 @@ HPALETTE CreatePalette(LOGPALETTE *lplgpl)
 	hPalette->logicalPalette = lplgpl;
 	return hPalette;
 }
+
+/**
+ * Create a new solid brush.
+ * http://msdn.microsoft.com/en-us/library/dd183518/
+ * @param crColor brush color
+ * @return new brush
+ */
 
 HBRUSH CreateSolidBrush(COLORREF crColor)
 {
@@ -169,6 +231,13 @@ HBRUSH CreateSolidBrush(COLORREF crColor)
 	return hBrush;
 }
 
+/**
+ * Create a new pattern brush.
+ * http://msdn.microsoft.com/en-us/library/dd183508/
+ * @param hbmp pattern bitmap
+ * @return new brush
+ */
+
 HBRUSH CreatePatternBrush(HBITMAP hbmp)
 {
 	HBRUSH hBrush = (HBRUSH) malloc(sizeof(BRUSH));
@@ -177,6 +246,16 @@ HBRUSH CreatePatternBrush(HBITMAP hbmp)
 	hBrush->pattern = hbmp;
 	return hBrush;
 }
+
+/**
+ * Create a region from rectangular coordinates.
+ * http://msdn.microsoft.com/en-us/library/dd183514/
+ * @param nLeftRect x1
+ * @param nTopRect y1
+ * @param nRightRect x2
+ * @param nBottomRect y2
+ * @return new region
+ */
 
 HRGN CreateRectRgn(int nLeftRect, int nTopRect, int nRightRect, int nBottomRect)
 {
@@ -190,6 +269,15 @@ HRGN CreateRectRgn(int nLeftRect, int nTopRect, int nRightRect, int nBottomRect)
 	return hRgn;
 }
 
+/**
+ * Create a new rectangle.
+ * @param xLeft x1
+ * @param yTop y1
+ * @param xRight x2
+ * @param yBottom y2
+ * @return new rectangle
+ */
+
 HRECT CreateRect(int xLeft, int yTop, int xRight, int yBottom)
 {
 	HRECT hRect = (HRECT) malloc(sizeof(RECT));
@@ -201,6 +289,12 @@ HRECT CreateRect(int xLeft, int yTop, int xRight, int yBottom)
 	return hRect;
 }
 
+/**
+ * Convert a rectangle to a region.
+ * @param rect source rectangle
+ * @param rgn destination region
+ */
+
 void RectToRgn(HRECT rect, HRGN rgn)
 {
 	rgn->x = rect->left;
@@ -208,6 +302,15 @@ void RectToRgn(HRECT rect, HRGN rgn)
 	rgn->w = rect->right - rect->left + 1;
 	rgn->h = rect->bottom - rect->top + 1;
 }
+
+/**
+ * Convert rectangular coordinates to a region.
+ * @param left x1
+ * @param top y1
+ * @param right x2
+ * @param bottom y2
+ * @param rgn destination region
+ */
 
 void CRectToRgn(int left, int top, int right, int bottom, HRGN rgn)
 {
@@ -217,6 +320,15 @@ void CRectToRgn(int left, int top, int right, int bottom, HRGN rgn)
 	rgn->h = bottom - top + 1;
 }
 
+/**
+ * Convert a rectangle to region coordinates.
+ * @param rect source rectangle
+ * @param x x1
+ * @param y y1
+ * @param w width
+ * @param h height
+ */
+
 void RectToCRgn(HRECT rect, int *x, int *y, int *w, int *h)
 {
 	*x = rect->left;
@@ -224,6 +336,18 @@ void RectToCRgn(HRECT rect, int *x, int *y, int *w, int *h)
 	*w = rect->right - rect->left + 1;
 	*h = rect->bottom - rect->top + 1;
 }
+
+/**
+ * Convert rectangular coordinates to region coordinates.
+ * @param left x1
+ * @param top y1
+ * @param right x2
+ * @param bottom y2
+ * @param x x1
+ * @param y y1
+ * @param w width
+ * @param h height
+ */
 
 void CRectToCRgn(int left, int top, int right, int bottom, int *x, int *y, int *w, int *h)
 {
@@ -233,6 +357,12 @@ void CRectToCRgn(int left, int top, int right, int bottom, int *x, int *y, int *
 	*h = bottom - top + 1;
 }
 
+/**
+ * Convert a region to a rectangle.
+ * @param rgn source region
+ * @param rect destination rectangle
+ */
+
 void RgnToRect(HRGN rgn, HRECT rect)
 {
 	rect->left = rgn->x;
@@ -240,6 +370,15 @@ void RgnToRect(HRGN rgn, HRECT rect)
 	rect->right = rgn->x + rgn->w - 1;
 	rect->bottom = rgn->y + rgn->h - 1;
 }
+
+/**
+ * Convert region coordinates to a rectangle.
+ * @param x x1
+ * @param y y1
+ * @param w width
+ * @param h height
+ * @param rect destination rectangle
+ */
 
 void CRgnToRect(int x, int y, int w, int h, HRECT rect)
 {
@@ -249,6 +388,15 @@ void CRgnToRect(int x, int y, int w, int h, HRECT rect)
 	rect->bottom = y + h - 1;
 }
 
+/**
+ * Convert a region to rectangular coordinates.
+ * @param rgn source region
+ * @param left x1
+ * @param top y1
+ * @param right x2
+ * @param bottom y2
+ */
+
 void RgnToCRect(HRGN rgn, int *left, int *top, int *right, int *bottom)
 {
 	*left = rgn->x;
@@ -257,6 +405,18 @@ void RgnToCRect(HRGN rgn, int *left, int *top, int *right, int *bottom)
 	*bottom = rgn->y + rgn->h - 1;
 }
 
+/**
+ * Convert region coordinates to rectangular coordinates.
+ * @param x x1
+ * @param y y1
+ * @param w width
+ * @param h height
+ * @param left x1
+ * @param top y1
+ * @param right x2
+ * @param bottom y2
+ */
+
 void CRgnToCRect(int x, int y, int w, int h, int *left, int *top, int *right, int *bottom)
 {
 	*left = x;
@@ -264,6 +424,17 @@ void CRgnToCRect(int x, int y, int w, int h, int *left, int *top, int *right, in
 	*right = x + w - 1;
 	*bottom = y + h - 1;
 }
+
+/**
+ * Check if copying would involve overlapping regions
+ * @param x x1
+ * @param y y1
+ * @param width width
+ * @param height height
+ * @param srcx source x1
+ * @param srcy source y1
+ * @return 1 if there is an overlap, 0 otherwise
+ */
 
 int CopyOverlap(int x, int y, int width, int height, int srcx, int srcy)
 {
@@ -276,6 +447,14 @@ int CopyOverlap(int x, int y, int width, int height, int srcx, int srcy)
 	return (dst.right > src.left && dst.left < src.right &&
 		dst.bottom > src.top && dst.top < src.bottom) ? 1 : 0;
 }
+
+/**
+ * Set current foreground draw mode.
+ * http://msdn.microsoft.com/en-us/library/dd145088/
+ * @param hdc device context
+ * @param fnDrawMode draw mode
+ * @return previous draw mode
+ */
 
 int SetROP2(HDC hdc, int fnDrawMode)
 {
@@ -339,6 +518,15 @@ bresenham(HDC hdc, int x1, int y1, int x2, int y2)
 	}
 }
 
+/**
+ * Draw a line from the current position to the given position.
+ * http://msdn.microsoft.com/en-us/library/dd145029/
+ * @param hdc device context
+ * @param nXEnd ending x position
+ * @param nYEnd ending y position
+ * @return 1 if successful, 0 otherwise
+ */
+
 int LineTo(HDC hdc, int nXEnd, int nYEnd)
 {	
 	printf("LineTo: posX:%d posY:%d nXEnd:%d nYEnd:%d\n",
@@ -346,7 +534,7 @@ int LineTo(HDC hdc, int nXEnd, int nYEnd)
 
 	/*
 	 * According to this MSDN article, LineTo uses a modified version of Bresenham:
-	 * http://msdn.microsoft.com/en-us/library/dd145027(VS.85).aspx
+	 * http://msdn.microsoft.com/en-us/library/dd145027/
 	 *
 	 * However, since I couldn't find the specifications of this modified algorithm,
 	 * we're going to use the original Bresenham line drawing algorithm for now
@@ -357,12 +545,31 @@ int LineTo(HDC hdc, int nXEnd, int nYEnd)
 	return 1;
 }
 
+/**
+ * Move pen from the current device context to a new position.
+ * @param hdc device context
+ * @param X x position
+ * @param Y y position
+ * @return 1 if successful, 0 otherwise
+ */
+
 int MoveTo(HDC hdc, int X, int Y)
 {
 	hdc->pen->posX = X;
 	hdc->pen->posY = Y;
 	return 1;
 }
+
+/**
+ * Set the coordinates of a given rectangle.
+ * http://msdn.microsoft.com/en-us/library/dd145085/
+ * @param rc rectangle
+ * @param xLeft x1
+ * @param yTop y1
+ * @param xRight x2
+ * @param yBottom y2
+ * @return 1 if successful, 0 otherwise
+ */
 
 int SetRect(HRECT rc, int xLeft, int yTop, int xRight, int yBottom)
 {
@@ -372,6 +579,16 @@ int SetRect(HRECT rc, int xLeft, int yTop, int xRight, int yBottom)
 	rc->bottom = yBottom;
 	return 1;
 }
+
+/**
+ * Set the coordinates of a given region.
+ * @param hRgn region
+ * @param nXLeft x1
+ * @param nYLeft y1
+ * @param nWidth width
+ * @param nHeight height
+ * @return
+ */
 
 int SetRgn(HRGN hRgn, int nXLeft, int nYLeft, int nWidth, int nHeight)
 {
@@ -383,6 +600,16 @@ int SetRgn(HRGN hRgn, int nXLeft, int nYLeft, int nWidth, int nHeight)
 	return 0;
 }
 
+/**
+ * Convert rectangular coordinates to a region
+ * @param hRgn destination region
+ * @param nLeftRect x1
+ * @param nTopRect y1
+ * @param nRightRect x2
+ * @param nBottomRect y2
+ * @return
+ */
+
 int SetRectRgn(HRGN hRgn, int nLeftRect, int nTopRect, int nRightRect, int nBottomRect)
 {
 	CRectToRgn(nLeftRect, nTopRect, nRightRect, nBottomRect, hRgn);
@@ -390,15 +617,38 @@ int SetRectRgn(HRGN hRgn, int nLeftRect, int nTopRect, int nRightRect, int nBott
 	return 0;
 }
 
+/**
+ * Set the current clipping region coordinates.
+ * @param hdc device context
+ * @param nXLeft x1
+ * @param nYLeft y1
+ * @param nWidth width
+ * @param nHeight height
+ * @return
+ */
+
 int SetClipRgn(HDC hdc, int nXLeft, int nYLeft, int nWidth, int nHeight)
 {
 	return SetRgn(hdc->clip, nXLeft, nYLeft, nWidth, nHeight);
 }
 
+/**
+ * Get the current clipping region.
+ * http://msdn.microsoft.com/en-us/library/dd144866/
+ * @param hdc device context
+ * @return clipping region
+ */
+
 HRGN GetClipRgn(HDC hdc)
 {
 	return hdc->clip;
 }
+
+/**
+ * Set the current clipping region to null.
+ * @param hdc device context
+ * @return
+ */
 
 int SetNullClipRgn(HDC hdc)
 {
@@ -406,6 +656,18 @@ int SetNullClipRgn(HDC hdc)
 	hdc->clip->null = 1;
 	return 0;
 }
+
+/**
+ * Clip coordinates according to clipping region
+ * @param hdc device context
+ * @param x x1
+ * @param y y1
+ * @param w width
+ * @param h height
+ * @param srcx source x1
+ * @param srcy source y1
+ * @return 1 if there is something to draw, 0 otherwise
+ */
 
 int ClipCoords(HDC hdc, int *x, int *y, int *w, int *h, int *srcx, int *srcy)
 {
@@ -509,6 +771,17 @@ int ClipCoords(HDC hdc, int *x, int *y, int *w, int *h, int *srcx, int *srcy)
 	return draw;
 }
 
+/**
+ * Invalidate a given region, such that it is redrawn on the next region update.
+ * http://msdn.microsoft.com/en-us/library/dd145003/
+ * @param hdc device context
+ * @param x x1
+ * @param y y1
+ * @param w width
+ * @param h height
+ * @return
+ */
+
 int InvalidateRegion(HDC hdc, int x, int y, int w, int h)
 {
 	RECT inv;
@@ -561,6 +834,14 @@ int InvalidateRegion(HDC hdc, int x, int y, int w, int h)
 	return 0;
 }
 
+/**
+ * Compare two regions for equality.
+ * http://msdn.microsoft.com/en-us/library/dd162700/
+ * @param hSrcRgn1 first region
+ * @param hSrcRgn2 second region
+ * @return 1 if both regions are equal, 0 otherwise
+ */
+
 int EqualRgn(HRGN hSrcRgn1, HRGN hSrcRgn2)
 {
 	if ((hSrcRgn1->x == hSrcRgn2->x) &&
@@ -574,6 +855,13 @@ int EqualRgn(HRGN hSrcRgn1, HRGN hSrcRgn2)
 	return 0;
 }
 
+/**
+ * Copy coordinates from a rectangle to another rectangle
+ * @param dst destination rectangle
+ * @param src source rectangle
+ * @return 1 if successful, 0 otherwise
+ */
+
 int CopyRect(HRECT dst, HRECT src)
 {
 	dst->left = src->left;
@@ -582,6 +870,15 @@ int CopyRect(HRECT dst, HRECT src)
 	dst->bottom = src->bottom;
 	return 1;
 }
+
+/**
+ * Check if a point is inside a rectangle.
+ * http://msdn.microsoft.com/en-us/library/dd162882/
+ * @param rc rectangle
+ * @param x point x position
+ * @param y point y position
+ * @return 1 if the point is inside, 0 otherwise
+ */
 
 int PtInRect(HRECT rc, int x, int y)
 {
@@ -601,10 +898,25 @@ int PtInRect(HRECT rc, int x, int y)
 	return 0;
 }
 
+/**
+ * Get the current background color.
+ * http://msdn.microsoft.com/en-us/library/dd144852/
+ * @param hdc device context
+ * @return background color
+ */
+
 COLORREF GetBkColor(HDC hdc)
 {
 	return hdc->bkColor;
 }
+
+/**
+ * Set the current background color.
+ * http://msdn.microsoft.com/en-us/library/dd162964/
+ * @param hdc device color
+ * @param crColor new background color
+ * @return previous background color
+ */
 
 COLORREF SetBkColor(HDC hdc, COLORREF crColor)
 {
@@ -613,12 +925,28 @@ COLORREF SetBkColor(HDC hdc, COLORREF crColor)
 	return previousBkColor;
 }
 
+/**
+ * Set the current text color.
+ * http://msdn.microsoft.com/en-us/library/dd145093/
+ * @param hdc device context
+ * @param crColor new text color
+ * @return previous text color
+ */
+
 COLORREF SetTextColor(HDC hdc, COLORREF crColor)
 {
 	COLORREF previousTextColor = hdc->textColor;
 	hdc->textColor = crColor;
 	return previousTextColor;
 }
+
+/**
+ * Set the current background mode.
+ * http://msdn.microsoft.com/en-us/library/dd162965/
+ * @param hdc device context
+ * @param iBkMode background mode
+ * @return
+ */
 
 int SetBkMode(HDC hdc, int iBkMode)
 {
@@ -630,6 +958,15 @@ int SetBkMode(HDC hdc, int iBkMode)
 	return 0;
 }
 
+/**
+ * Get pixel at the given coordinates.
+ * http://msdn.microsoft.com/en-us/library/dd144909/
+ * @param hdc device context
+ * @param nXPos pixel x position
+ * @param nYPos pixel y position
+ * @return pixel color
+ */
+
 COLORREF GetPixel(HDC hdc, int nXPos, int nYPos)
 {
 	HBITMAP hBmp = (HBITMAP) hdc->selectedObject;
@@ -637,12 +974,31 @@ COLORREF GetPixel(HDC hdc, int nXPos, int nYPos)
 	return (COLORREF) *colorp;
 }
 
+/**
+ * Set pixel at the given coordinates.
+ * http://msdn.microsoft.com/en-us/library/dd145078/
+ * @param hdc device context
+ * @param X pixel x position
+ * @param Y pixel y position
+ * @param crColor new pixel color
+ * @return
+ */
+
 COLORREF SetPixel(HDC hdc, int X, int Y, COLORREF crColor)
 {
 	HBITMAP hBmp = (HBITMAP) hdc->selectedObject;
 	*((COLORREF*)&(hBmp->data[(Y * hBmp->width * hdc->bytesPerPixel) + X * hdc->bytesPerPixel])) = crColor;
 	return 0;
 }
+
+/**
+ * Fill a rectangle with the given brush.
+ * http://msdn.microsoft.com/en-us/library/dd162719/
+ * @param hdc device context
+ * @param rect rectangle
+ * @param hbr brush
+ * @return 1 if successful, 0 otherwise
+ */
 
 int FillRect(HDC hdc, HRECT rect, HBRUSH hbr)
 {
@@ -659,6 +1015,21 @@ int FillRect(HDC hdc, HRECT rect, HBRUSH hbr)
 	}
 }
 
+/**
+ * Perform a bit blit operation on the given pixel buffers.
+ * http://msdn.microsoft.com/en-us/library/dd183370/
+ * @param hdcDest destination device context
+ * @param nXDest destination x1
+ * @param nYDest destination y1
+ * @param nWidth width
+ * @param nHeight height
+ * @param hdcSrc source device context
+ * @param nXSrc source x1
+ * @param nYSrc source y1
+ * @param rop raster operation code
+ * @return 1 if successful, 0 otherwise
+ */
+
 int BitBlt(HDC hdcDest, int nXDest, int nYDest, int nWidth, int nHeight, HDC hdcSrc, int nXSrc, int nYSrc, int rop)
 {
 	switch (hdcDest->bitsPerPixel)
@@ -674,6 +1045,18 @@ int BitBlt(HDC hdcDest, int nXDest, int nYDest, int nWidth, int nHeight, HDC hdc
 	}
 }
 
+/**
+ * Perform a pattern blit operation on the given pixel buffer.
+ * http://msdn.microsoft.com/en-us/library/dd162778/
+ * @param hdc device context
+ * @param nXLeft x1
+ * @param nYLeft y1
+ * @param nWidth width
+ * @param nHeight height
+ * @param rop raster operation code
+ * @return 1 if successful, 0 otherwise
+ */
+
 int PatBlt(HDC hdc, int nXLeft, int nYLeft, int nWidth, int nHeight, int rop)
 {
 	switch (hdc->bitsPerPixel)
@@ -688,6 +1071,14 @@ int PatBlt(HDC hdc, int nXLeft, int nYLeft, int nWidth, int nHeight, int rop)
 			return 0;
 	}
 }
+
+/**
+ * Select a GDI object in the current device context.
+ * http://msdn.microsoft.com/en-us/library/dd162957/
+ * @param hdc device context
+ * @param hgdiobj new selected GDI object
+ * @return previous selected GDI object
+ */
 
 HGDIOBJ SelectObject(HDC hdc, HGDIOBJ hgdiobj)
 {
@@ -726,6 +1117,13 @@ HGDIOBJ SelectObject(HDC hdc, HGDIOBJ hgdiobj)
 	
 	return previousSelectedObject;
 }
+
+/**
+ * Delete a GDI object.
+ * http://msdn.microsoft.com/en-us/library/dd183539/
+ * @param hgdiobj GDI object
+ * @return 1 if successful, 0 otherwise
+ */
 
 int DeleteObject(HGDIOBJ hgdiobj)
 {
@@ -782,6 +1180,13 @@ int DeleteObject(HGDIOBJ hgdiobj)
 	
 	return 1;
 }
+
+/**
+ * Delete device context.
+ * http://msdn.microsoft.com/en-us/library/dd183533/
+ * @param hdc device context
+ * @return 1 if successful, 0 otherwise
+ */
 
 int DeleteDC(HDC hdc)
 {
