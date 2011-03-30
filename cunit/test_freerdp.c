7bbde4fb3bc3a781364d0a626c49bc165cc507fd
Marc-André Moreau <marcandre.moreau@gmail.com>
2011-03-30 00:38:09 -0400

    changed test bitmaps for BitBlt to 8bpp instead of 16bpp

--- cunit/test_freerdp.c
+++ cunit/test_freerdp.c
@@ -13,7 +13,8 @@ int main(int argc, char* argv[])
 		return CU_get_error();
 
 	//add_credssp_suite();
-	add_ntlmssp_suite();
+	//add_ntlmssp_suite();
+	add_libfreerdpgdi_suite();
 
 	CU_basic_set_mode(CU_BRM_VERBOSE);
 	CU_basic_run_tests();
