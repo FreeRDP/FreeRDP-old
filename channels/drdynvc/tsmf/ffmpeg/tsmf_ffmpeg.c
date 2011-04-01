cc835cf82ba13e7b6a721dff2e9b428db8e5c5c7
Vic Lee <llyzs@163.com>
2011-04-01 15:24:19 +0800

    tsmf/ffmpeg: add initial skeleton based on the 'copy' decoder

---
+++ channels/drdynvc/tsmf/ffmpeg/tsmf_ffmpeg.c
@@ -0,0 +1,242 @@
+/*
+   FreeRDP: A Remote Desktop Protocol client.
+   Video Redirection Virtual Channel - FFmpeg Decoder
+
+   Copyright 2010-2011 Vic Lee
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <libavcodec/avcodec.h>
+#include <libswscale/swscale.h>
+#include "tsmf_constants.h"
+#include "tsmf_decoder.h"
+
+/* Compatibility with older FFmpeg */
+#if LIBAVUTIL_VERSION_MAJOR < 50
+#define AVMEDIA_TYPE_VIDEO 0
+#define AVMEDIA_TYPE_AUDIO 1
+#endif
+
+typedef struct _TSMFFFmpegDecoder
+{
+	ITSMFDecoder iface;
+
+	int media_type;
+	enum CodecID codec_id;
+	AVCodecContext * codec_context;
+	AVCodec * codec;
+	int prepared;
+} TSMFFFmpegDecoder;
+
+static int
+tsmf_ffmpeg_init_context(ITSMFDecoder * decoder)
+{
+	TSMFFFmpegDecoder * mdecoder = (TSMFFFmpegDecoder *) decoder;
+
+	mdecoder->codec_context = avcodec_alloc_context();
+	if (!mdecoder->codec_context)
+	{
+		LLOGLN(0, ("tsmf_ffmpeg_init_context: avcodec_alloc_context failed."));
+		return 1;
+	}
+
+	return 0;
+}
+
+static int
+tsmf_ffmpeg_init_video_stream(ITSMFDecoder * decoder, const TS_AM_MEDIA_TYPE * media_type)
+{
+	TSMFFFmpegDecoder * mdecoder = (TSMFFFmpegDecoder *) decoder;
+
+	mdecoder->codec_context->width = media_type->Width;
+	mdecoder->codec_context->height = media_type->Height;
+	mdecoder->codec_context->bit_rate = media_type->BitRate;
+	mdecoder->codec_context->time_base.den = media_type->SamplesPerSecond.Numerator;
+	mdecoder->codec_context->time_base.num = media_type->SamplesPerSecond.Denominator;
+
+	mdecoder->codec_context->gop_size = 12;
+	mdecoder->codec_context->pix_fmt = PIX_FMT_YUV420P;
+
+	return 0;
+}
+
+static int
+tsmf_ffmpeg_init_audio_stream(ITSMFDecoder * decoder, const TS_AM_MEDIA_TYPE * media_type)
+{
+	TSMFFFmpegDecoder * mdecoder = (TSMFFFmpegDecoder *) decoder;
+
+	mdecoder->codec_context->sample_rate = media_type->SamplesPerSecond.Numerator;
+	mdecoder->codec_context->bit_rate = media_type->BitRate;
+	mdecoder->codec_context->channels = media_type->Channels;
+	mdecoder->codec_context->block_align = media_type->BlockAlign;
+
+	switch (media_type->BitsPerSample)
+	{
+		case 8:
+			mdecoder->codec_context->sample_fmt = SAMPLE_FMT_U8;
+			break;
+		case 16:
+			mdecoder->codec_context->sample_fmt = SAMPLE_FMT_S16;
+			break;
+		case 32:
+			mdecoder->codec_context->sample_fmt = SAMPLE_FMT_S32;
+			break;
+	}
+
+	return 0;
+}
+
+static int
+tsmf_ffmpeg_init_stream(ITSMFDecoder * decoder, const TS_AM_MEDIA_TYPE * media_type)
+{
+	TSMFFFmpegDecoder * mdecoder = (TSMFFFmpegDecoder *) decoder;
+
+	mdecoder->codec = avcodec_find_decoder(mdecoder->codec_id);
+	if (!mdecoder->codec)
+	{
+		LLOGLN(0, ("tsmf_ffmpeg_init_stream: avcodec_find_decoder failed."));
+		return 1;
+	}
+
+	mdecoder->codec_context->codec_id = mdecoder->codec_id;
+	mdecoder->codec_context->codec_type = mdecoder->media_type;
+
+	if (mdecoder->media_type == AVMEDIA_TYPE_VIDEO)
+	{
+		if (tsmf_ffmpeg_init_video_stream(decoder, media_type))
+			return 1;
+	}
+	else if (mdecoder->media_type == AVMEDIA_TYPE_AUDIO)
+	{
+		if (tsmf_ffmpeg_init_audio_stream(decoder, media_type))
+			return 1;
+	}
+
+	if (media_type->ExtraData)
+	{
+		mdecoder->codec_context->extradata_size = media_type->ExtraDataSize;
+		mdecoder->codec_context->extradata = malloc(mdecoder->codec_context->extradata_size);
+		memcpy(mdecoder->codec_context->extradata, media_type->ExtraData, media_type->ExtraDataSize);
+	}
+
+	if (mdecoder->codec->capabilities & CODEC_CAP_TRUNCATED)
+		mdecoder->codec_context->flags |= CODEC_FLAG_TRUNCATED;
+
+	return 0;
+}
+
+static int
+tsmf_ffmpeg_prepare(ITSMFDecoder * decoder)
+{
+	TSMFFFmpegDecoder * mdecoder = (TSMFFFmpegDecoder *) decoder;
+
+	if (avcodec_open(mdecoder->codec_context, mdecoder->codec) < 0)
+	{
+		LLOGLN(0, ("tsmf_ffmpeg_prepare: avcodec_open failed."));
+		return 1;
+	}
+
+	mdecoder->prepared = 1;
+
+	return 0;
+}
+
+static int
+tsmf_ffmpeg_set_format(ITSMFDecoder * decoder, const TS_AM_MEDIA_TYPE * media_type)
+{
+	TSMFFFmpegDecoder * mdecoder = (TSMFFFmpegDecoder *) decoder;
+
+	switch (media_type->MajorType)
+	{
+		case TSMF_MAJOR_TYPE_VIDEO:
+			mdecoder->media_type = AVMEDIA_TYPE_VIDEO;
+			break;
+		case TSMF_MAJOR_TYPE_AUDIO:
+			mdecoder->media_type = AVMEDIA_TYPE_AUDIO;
+			break;
+		default:
+			return 1;
+	}
+	switch (media_type->SubType)
+	{
+		case TSMF_SUB_TYPE_WVC1:
+			mdecoder->codec_id = CODEC_ID_VC1;
+			break;
+		case TSMF_SUB_TYPE_WMA2:
+			mdecoder->codec_id = CODEC_ID_WMAV2;
+			break;
+		case TSMF_SUB_TYPE_WMA9:
+			mdecoder->codec_id = CODEC_ID_WMAPRO;
+			break;
+		default:
+			return 1;
+	}
+
+	if (tsmf_ffmpeg_init_context(decoder))
+		return 1;
+	if (tsmf_ffmpeg_init_stream(decoder, media_type))
+		return 1;
+	if (tsmf_ffmpeg_prepare(decoder))
+		return 1;
+
+	return 0;
+}
+
+static int
+tsmf_ffmpeg_decode(ITSMFDecoder * decoder, const uint8 * data, uint32 data_size, uint32 extensions,
+	uint8 ** decoded_data, uint32 * decoded_size)
+{
+	*decoded_data = NULL;
+	*decoded_size = 0;
+
+	return 0;
+}
+
+static void
+tsmf_ffmpeg_free(ITSMFDecoder * decoder)
+{
+	TSMFFFmpegDecoder * mdecoder = (TSMFFFmpegDecoder *) decoder;
+
+	if (mdecoder->codec_context)
+	{
+		if (mdecoder->prepared)
+			avcodec_close(mdecoder->codec_context);
+		if (mdecoder->codec_context->extradata)
+			free(mdecoder->codec_context->extradata);
+		av_free(mdecoder->codec_context);
+	}
+	free(decoder);
+}
+
+ITSMFDecoder *
+TSMFDecoderEntry(void)
+{
+	TSMFFFmpegDecoder * decoder;
+
+	avcodec_init();
+	avcodec_register_all();
+
+	decoder = malloc(sizeof(TSMFFFmpegDecoder));
+	memset(decoder, 0, sizeof(TSMFFFmpegDecoder));
+
+	decoder->iface.SetFormat = tsmf_ffmpeg_set_format;
+	decoder->iface.Decode = tsmf_ffmpeg_decode;
+	decoder->iface.Free = tsmf_ffmpeg_free;
+
+	return (ITSMFDecoder *) decoder;
+}
+
