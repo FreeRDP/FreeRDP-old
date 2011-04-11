a1849a7d8b0e393c7a81f151d42499d8ad82d29a
Marc-André Moreau <marcandre.moreau@gmail.com>
2011-04-11 02:48:43 -0400

    Started implementing Ellipse() along with unit test that fails at this point

--- libgdi/gdi_shape.c
+++ libgdi/gdi_shape.c
@@ -32,6 +32,105 @@
 
 pFillRect FillRect_[5];
 
+static void Ellipse_Bresenham(HDC hdc, int x1, int y1, int x2, int y2)
+{
+	int a, b, c;
+	int e, e2;
+	int dx, dy;
+
+	HBITMAP bmp;
+	uint8 pixel8;
+	uint16 pixel16;
+	uint32 pixel32;
+	int bpp = hdc->bitsPerPixel;
+
+	a = (x1 < x2) ? x2 - x1 : x1 - x2;
+	b = (y1 < y2) ? y2 - y1 : y1 - y2;
+	c = b & 1;
+
+	dx = 4 * (1 - a) * b * b;
+	dy = 4 * (c + 1) * a * a;
+
+	if (x2 > x1)
+	{
+		x2 = x1;
+		x2 += a;
+	}
+
+	if (y2 > y1)
+		y2 = y1;
+
+	y1 += (b + 1) / 2;
+	y2 = y1 - c;
+
+	a *= 8 * a;
+	c = 8 * b * b;
+
+	pixel8 = pixel16 = pixel32 = 0;
+	bmp = (HBITMAP) hdc->selectedObject;
+
+	do
+	{
+		if (bpp == 32)
+		{
+			SetPixel_32bpp(bmp, x2, y1, pixel32);
+			SetPixel_32bpp(bmp, x1, y1, pixel32);
+			SetPixel_32bpp(bmp, x1, y2, pixel32);
+			SetPixel_32bpp(bmp, x2, y2, pixel32);
+		}
+		else if (bpp == 16)
+		{
+			SetPixel_16bpp(bmp, x2, y1, pixel16);
+			SetPixel_16bpp(bmp, x1, y1, pixel16);
+			SetPixel_16bpp(bmp, x1, y2, pixel16);
+			SetPixel_16bpp(bmp, x2, y2, pixel16);
+		}
+		else if (bpp == 8)
+		{
+			SetPixel_8bpp(bmp, x2, y1, pixel8);
+			SetPixel_8bpp(bmp, x1, y1, pixel8);
+			SetPixel_8bpp(bmp, x1, y2, pixel8);
+			SetPixel_8bpp(bmp, x2, y2, pixel8);
+		}
+
+		e2 = 2 * e;
+
+		if (e2 >= dx)
+		{
+			x1++;
+			x2--;
+			e += dx += c;
+		}
+
+		if (e2 >= dy)
+		{
+			y1++;
+			y2--;
+			e += dy += a;
+		}
+	}
+	while (x1 <= x2);
+
+	while (y1 - y2 < b)
+	{
+		if (bpp == 32)
+		{
+			SetPixel_32bpp(bmp, x1 - 1, ++y1, pixel32);
+			SetPixel_32bpp(bmp, x1 - 1, --y2, pixel32);
+		}
+		else if (bpp == 16)
+		{
+			SetPixel_16bpp(bmp, x1 - 1, ++y1, pixel16);
+			SetPixel_16bpp(bmp, x1 - 1, --y2, pixel16);
+		}
+		else if (bpp == 8)
+		{
+			SetPixel_8bpp(bmp, x1 - 1, ++y1, pixel8);
+			SetPixel_8bpp(bmp, x1 - 1, --y2, pixel8);
+		}
+	}
+}
+
 /**
  * Draw an ellipse
  * @param hdc device context
@@ -43,6 +142,7 @@ pFillRect FillRect_[5];
  */
 int Ellipse(HDC hdc, int nLeftRect, int nTopRect, int nRightRect, int nBottomRect)
 {
+	Ellipse_Bresenham(hdc, nLeftRect, nTopRect, nRightRect, nBottomRect);
 	return 1;
 }
 
