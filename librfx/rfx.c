d89dcb40c16f3db33caf239b126944295bdad8d6
Vic Lee <llyzs@163.com>
2011-05-11 14:56:49 +0800

    librfx: add initial main rfx module.

---
+++ librfx/rfx.c
@@ -0,0 +1,50 @@
+/*
+   FreeRDP: A Remote Desktop Protocol client.
+   RemoteFX Codec Library
+
+   Copyright 2011 Vic Lee
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "rfx_decode.h"
+#include "rfx.h"
+
+struct _RFX_CONTEXT
+{
+	int flags;
+	RLGR_MODE mode;
+	int * quants;
+};
+
+RFX_CONTEXT *
+rfx_context_new(void)
+{
+	RFX_CONTEXT * context;
+
+	context = (RFX_CONTEXT *) malloc(sizeof(RFX_CONTEXT));
+	memset(context, 0, sizeof(RFX_CONTEXT));
+	return context;
+}
+
+void
+rfx_context_free(RFX_CONTEXT * context)
+{
+	if (context->quants)
+		free(context->quants);
+	free(context);
+}
+
