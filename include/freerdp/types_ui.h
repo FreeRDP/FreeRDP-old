a9eb096bbf1050efdaed992922c234fe45f1660f
Vic Lee <llyzs@163.com>
2011-05-13 12:06:00 +0800

    Separate base types into freerdp/types_base.h

--- include/freerdp/types_ui.h
+++ include/freerdp/types_ui.h
@@ -23,19 +23,7 @@
 #ifndef __TYPES_UI_H
 #define __TYPES_UI_H
 
-typedef unsigned char uint8;
-typedef signed char sint8;
-typedef unsigned short uint16;
-typedef signed short sint16;
-typedef unsigned int uint32;
-typedef signed int sint32;
-#ifdef _WIN32
-typedef unsigned __int64 uint64;
-typedef signed __int64 sint64;
-#else
-typedef unsigned long long uint64;
-typedef signed long long sint64;
-#endif
+#include <freerdp/types_base.h>
 
 typedef int RD_BOOL;
 typedef void *RD_HBITMAP;
